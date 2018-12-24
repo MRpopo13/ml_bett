f33e6c244ee4fabeee5ecfd5e43025bb54b53fb5
Bakari Nouhou <bakari.nouhou@engie.com>
2018-12-24 17:12:45 +0100

    add equilibrium datas set

---
+++ equilibrate_dataset.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+## 3.04	3.29	2.3	1.92	1.85	4.0	0.0	0.0	0.0	0.0	2	1	0.0	1	2	1	0	3
+## // odd_h odds_d odds_a odds_o odds_u pow_eq HDA_poiss    HDA_avg HDA_avg_wht HDA_poiss_scor  mean_h_goal mean_a_goals   h_conc_pg h_scor_pg h_point_pg a_conc_pg a_scor_pg a_point_pg HDA_res  diff_goals  h_goal  a_goal  ov_under    goals
+##     1     2      3      4       5       6         7         8       9           10              11          12              13          14          15      16      17          18          19      20          21      22      23          24
+
+def equalize_dataset(name_file):
+    dataset = np.loadtxt(name_file, delimiter="\t")
+
+    eq_dataset = [[data for data in dataset if data[18] == 0],
+                  [data for data in dataset if data[18] == 1],
+                  [data for data in dataset if data[18] == 2]]
+
+    size_max = min([len(eq) for eq in eq_dataset])
+    clean_dataset = [eq[:size_max] for eq in eq_dataset]
+    return np.concatenate((clean_dataset[0], clean_dataset[1], clean_dataset[2]), axis=0)
--- starter.py
+++ starter.py
@@ -6,6 +6,8 @@ from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
+from ml_bett.equilibrate_dataset import equalize_dataset
+
 ## 3.04	3.29	2.3	1.92	1.85	4.0	0.0	0.0	0.0	0.0	2	1	0.0	1	2	1	0	3
 ## // odd_h odds_d odds_a odds_o odds_u pow_eq HDA_poiss    HDA_avg HDA_avg_wht HDA_poiss_scor  mean_h_goal mean_a_goals   h_conc_pg h_scor_pg h_point_pg a_conc_pg a_scor_pg a_point_pg HDA_res  diff_goals  h_goal  a_goal  ov_under    goals
 ##     1     2      3      4       5       6         7         8       9           10              11          12              13          14          15      16      17          18          19      20          21      22      23          24
@@ -18,8 +20,9 @@ test_dataset = np.loadtxt("resources\\testing.csv", delimiter="\t")
 past_features = np.loadtxt("resources\\past_features.csv", delimiter="\t")
 past_results = np.loadtxt("resources\\past_results.csv", delimiter="\t")
 
-# odds = np.concatenate((dataset[:, :3], dataset[:, 6:8], dataset[:, 11:18]), axis=1)
-odds = dataset[:, 5:18]
+clean_data = equalize_dataset(name_file)
+# odds = np.concatenate((dataset[:, 13:16], dataset[:, 17], dataset[:, 6]), axis=1)
+odds = dataset[:, 5:10]
 results = dataset[:, 18]
 # seq = map(lambda x: 0 if x == 0 else 1, results)
 # seq = map(lambda x: x - 1, results)
@@ -28,30 +31,35 @@ results = np.fromiter(seq, dtype=np.int)
 
 clf_logistic = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
 clf_svc = svm.SVC(gamma=0.001, C=1., decision_function_shape='ovo', cache_size=1000, kernel='rbf')
-clf_neural = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                           hidden_layer_sizes=(5, 2), random_state=1)
+clf_svr = svm.SVR()
+clf_neural = MLPClassifier(solver='lbfgs', alpha=1e-3,
+                           activation='logistic', random_state=1)
 
-
-# clf_res.fit(training_data, training_res)
 
 def hist_proof(clf_model):
     predict_result = clf_model.predict(test_dataset)
     comp = [1 if predict_result[i] == past_results[i] else 0 for i in range(len(predict_result))]
     print("past accuracy is %.3f", sum(comp) / len(comp) * 100)
-    print([])
+    print([sum([1 if x == 0 else 0 for x in predict_result]) / len(comp) * 100,
+           sum([1 if x == 1 else 0 for x in predict_result]) / len(comp) * 100,
+           sum([1 if x == 2 else 0 for x in predict_result]) / len(comp) * 100])
 
 
 def evaluate_model(clf_model):
     x_train, x_test, y_train, y_test = model_selection.train_test_split(odds, results, test_size=0.3,
                                                                         random_state=0)
 
+    print([sum([1 if y == 0 else 0 for y in y_train]) / len(y_train) * 100,
+           sum([1 if y == 1 else 0 for y in y_train]) / len(y_train) * 100,
+           sum([1 if y == 2 else 0 for y in y_train]) / len(y_train) * 100])
+
+    print([sum([1 if y == 0 else 0 for y in y_test]) / len(y_test) * 100,
+           sum([1 if y == 1 else 0 for y in y_test]) / len(y_test) * 100,
+           sum([1 if y == 2 else 0 for y in y_test]) / len(y_test) * 100])
+
     x_scaler = preprocessing.StandardScaler()
     x_train = x_scaler.fit_transform(x_train)
     x_test = x_scaler.transform(x_test)
-
-    y_scaler = preprocessing.StandardScaler()
-    # y_train = y_scaler.fit_transform(y_train.reshape(-1,1))
-    # y_test = y_scaler.transform(y_test.reshape(-1,1))
 
     clf_model.fit(x_train, y_train)
 
@@ -60,9 +68,10 @@ def evaluate_model(clf_model):
     acc = clf_model.score(x_test, y_test)
     print(acc * 100)
 
-    hist_proof(clf_model)
-    print(clf_model.predict(test_dataset))
+    # hist_proof(clf_model)
+    # print(clf_model.predict(test_dataset))
+    # print(clf_model.predict_proba(test_dataset))
 
 
 if __name__ == '__main__':
-    evaluate_model(clf_svc)
+    evaluate_model(clf_neural)
