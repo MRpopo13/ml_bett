e271c541d8e902a788cb34d5438286aaab09eacf
Bakari Nouhou <bakari.nouhou@engie.com>
2018-12-26 18:18:52 +0100

    update date

--- starter.py
+++ starter.py
@@ -11,6 +11,11 @@ from ml_bett.equilibrate_dataset import equalize_dataset
 ## 3.04	3.29	2.3	1.92	1.85	4.0	0.0	0.0	0.0	0.0	2	1	0.0	1	2	1	0	3
 ## // odd_h odds_d odds_a odds_o odds_u pow_eq HDA_poiss    HDA_avg HDA_avg_wht HDA_poiss_scor  mean_h_goal mean_a_goals   h_conc_pg h_scor_pg h_point_pg a_conc_pg a_scor_pg a_point_pg HDA_res  diff_goals  h_goal  a_goal  ov_under    goals
 ##     1     2      3      4       5       6         7         8       9           10              11          12              13          14          15      16      17          18          19      20          21      22      23          24
+
+## 0	                1	        0
+##correct-result    correct-OU      correct-BTTS
+## 25                   26              27
+
 name_file = "resources\\features3.csv"
 dataset = np.loadtxt(name_file, delimiter="\t")
 # pow_eq HDA_poiss    HDA_avg HDA_avg_wht HDA_poiss_scor  mean_h_goal mean_a_goals   h_conc_pg h_scor_pg h_point_pg a_conc_pg a_scor_pg a_point_pg
@@ -22,8 +27,8 @@ past_results = np.loadtxt("resources\\past_results.csv", delimiter="\t")
 
 clean_data = equalize_dataset(name_file)
 # odds = np.concatenate((dataset[:, 13:16], dataset[:, 17], dataset[:, 6]), axis=1)
-odds = dataset[:, 5:10]
-results = dataset[:, 18]
+odds = dataset[:, 5:18]
+results = dataset[:, 24]
 # seq = map(lambda x: 0 if x == 0 else 1, results)
 # seq = map(lambda x: x - 1, results)
 seq = results
@@ -35,27 +40,42 @@ clf_svr = svm.SVR()
 clf_neural = MLPClassifier(solver='lbfgs', alpha=1e-3,
                            activation='logistic', random_state=1)
 
+results_test = past_results[:, 0]
+
 
 def hist_proof(clf_model):
     predict_result = clf_model.predict(test_dataset)
-    comp = [1 if predict_result[i] == past_results[i] else 0 for i in range(len(predict_result))]
-    print("past accuracy is %.3f", sum(comp) / len(comp) * 100)
-    print([sum([1 if x == 0 else 0 for x in predict_result]) / len(comp) * 100,
-           sum([1 if x == 1 else 0 for x in predict_result]) / len(comp) * 100,
-           sum([1 if x == 2 else 0 for x in predict_result]) / len(comp) * 100])
+    comp_res = [test_dataset[i][1] if predict_result[i] == 1 else test_dataset[i][1] for i in
+                range(len(test_dataset))]
+    predict_proba_result = clf_model.predict_proba(test_dataset)
+    predict_result = [(comp_res[i], results_test[i]) for i in range(len(predict_proba_result)) if
+                      predict_result[i] == 1]
+
+    # print([sum([1 if x == 0 else 0 for x in predict_result]) / len(predict_result) * 100,
+    #        sum([1 if x == 1 else 0 for x in predict_result]) / len(predict_result) * 100])
+
+    print("Number of correct pred ", len(predict_result))
+
+    comp = [1 if data[0] == data[1] else 0 for data in predict_result]
+
+    print("past accuracy is ", sum(comp) / len(comp) * 100)
+
+    print([sum([1 if x[0] == 0 else 0 for x in predict_result]) / len(predict_result) * 100,
+           sum([1 if x[0] == 1 else 0 for x in predict_result]) / len(predict_result) * 100,
+           sum([1 if x[0] == 2 else 0 for x in predict_result]) / len(predict_result) * 100])
 
 
 def evaluate_model(clf_model):
     x_train, x_test, y_train, y_test = model_selection.train_test_split(odds, results, test_size=0.3,
                                                                         random_state=0)
 
-    print([sum([1 if y == 0 else 0 for y in y_train]) / len(y_train) * 100,
-           sum([1 if y == 1 else 0 for y in y_train]) / len(y_train) * 100,
-           sum([1 if y == 2 else 0 for y in y_train]) / len(y_train) * 100])
-
-    print([sum([1 if y == 0 else 0 for y in y_test]) / len(y_test) * 100,
-           sum([1 if y == 1 else 0 for y in y_test]) / len(y_test) * 100,
-           sum([1 if y == 2 else 0 for y in y_test]) / len(y_test) * 100])
+    # print([sum([1 if y == 0 else 0 for y in y_train]) / len(y_train) * 100,
+    #        sum([1 if y == 1 else 0 for y in y_train]) / len(y_train) * 100,
+    #        sum([1 if y == 2 else 0 for y in y_train]) / len(y_train) * 100])
+    #
+    # print([sum([1 if y == 0 else 0 for y in y_test]) / len(y_test) * 100,
+    #        sum([1 if y == 1 else 0 for y in y_test]) / len(y_test) * 100,
+    #        sum([1 if y == 2 else 0 for y in y_test]) / len(y_test) * 100])
 
     x_scaler = preprocessing.StandardScaler()
     x_train = x_scaler.fit_transform(x_train)
@@ -68,10 +88,10 @@ def evaluate_model(clf_model):
     acc = clf_model.score(x_test, y_test)
     print(acc * 100)
 
-    # hist_proof(clf_model)
+    hist_proof(clf_model)
     # print(clf_model.predict(test_dataset))
     # print(clf_model.predict_proba(test_dataset))
 
 
 if __name__ == '__main__':
-    evaluate_model(clf_neural)
+    evaluate_model(clf_svc)
